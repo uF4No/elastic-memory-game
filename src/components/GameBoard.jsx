95edc0b1abbedc101279f43a54eaa7aa2ef0d538
Antonio <tonioufa@gmail.com>
2025-03-18 15:54:31 +0100

    more chains

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,14 +2,14 @@ import { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import Tile from './Tile';
 
-// Using local chain logo images with their corresponding website URLs
-const TILE_DATA = [
+// All available chain logo images with their corresponding website URLs
+const ALL_TILE_DATA = [
   {
     image: '/img/sophon.png',
     url: 'https://sophon.xyz'
   },
   {
-    image: '/img/lens-chain.png',
+    image: '/img/lens.png',
     url: 'https://lens.xyz'
   },
   {
@@ -35,6 +35,22 @@ const TILE_DATA = [
   {
     image: '/img/xsolla.png',
     url: 'https://xsolla.com'
+  },
+  {
+    image: '/img/wonder.png',
+    url: 'https://labs.wonder.fi/layer2'
+  },
+  {
+    image: '/img/zkcandy.png',
+    url: 'https://zkcandy.io'
+  },
+  {
+    image: '/img/zero.png',
+    url: 'https://zero.network'
+  },
+  {
+    image: '/img/grvt.png',
+    url: 'https://grvt.io'
   }
 ];
 
@@ -42,9 +58,19 @@ const GameBoard = ({ onGameComplete, onMovesUpdate }) => {
   const [tiles, setTiles] = useState([]);
   const [flippedTiles, setFlippedTiles] = useState([]);
   const [matchedPairs, setMatchedPairs] = useState([]);
+  const [selectedTileData, setSelectedTileData] = useState([]);
+
+  // Function to randomly select 8 unique tiles from ALL_TILE_DATA
+  const selectRandomTiles = useCallback(() => {
+    const shuffled = [...ALL_TILE_DATA].sort(() => Math.random() - 0.5);
+    return shuffled.slice(0, 8);
+  }, []);
 
   const initializeBoard = useCallback(() => {
-    const shuffledTiles = [...TILE_DATA, ...TILE_DATA]
+    const selectedTiles = selectRandomTiles();
+    setSelectedTileData(selectedTiles);
+    
+    const shuffledTiles = [...selectedTiles, ...selectedTiles]
       .map((data, index) => ({
         id: index,
         image: data.image,
@@ -53,10 +79,11 @@ const GameBoard = ({ onGameComplete, onMovesUpdate }) => {
         isMatched: false,
       }))
       .sort(() => Math.random() - 0.5);
+    
     setTiles(shuffledTiles);
     setFlippedTiles([]);
     setMatchedPairs([]);
-  }, []);
+  }, [selectRandomTiles]);
 
   useEffect(() => {
     initializeBoard();
@@ -83,7 +110,7 @@ const GameBoard = ({ onGameComplete, onMovesUpdate }) => {
         setFlippedTiles([]);
         onMovesUpdate((prev) => prev + 1);
 
-        if (matchedPairs.length + 1 === TILE_DATA.length) {
+        if (matchedPairs.length + 1 === selectedTileData.length) {
           onGameComplete();
         }
       } else {
